3a6de55ea330d91d34d2b27b84f6a8e18d35764b
Nomthandazo <damacm1133@DAs-MacBook-Air.local>
2026-01-14 14:38:13 +0200

    auth guad now doesnt allow access for users not logged in

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Movie } from 'src/app/models/movie.model';
+import { SignInRes } from 'src/app/models/user.model';
 import { MovieService } from 'src/app/services/movie.service';
+import { selectUser } from 'src/app/store/auth/auth.selectors';
+import { AuthState } from 'src/app/store/auth/auth.state';
 
 @Component({
   selector: 'app-favorites',
@@ -10,11 +15,24 @@ import { MovieService } from 'src/app/services/movie.service';
 })
 export class FavoritesComponent implements OnInit {
   favorites: Movie[] = [];
+  isUserSession: SignInRes | null = null;
 
-  constructor(private movieService: MovieService) {}
+  constructor(
+    private movieService: MovieService, 
+    private store: Store<AuthState>,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.favorites = this.movieService.getFavorites();
+
+    this.store.select(selectUser).subscribe(
+      userRes => this.isUserSession = userRes
+    )
+
+    if(this.isUserSession == null) {
+      this.router.navigate(["/signin"])
+    }
   }
 
   removeFromFavorites(movieId: string) {
